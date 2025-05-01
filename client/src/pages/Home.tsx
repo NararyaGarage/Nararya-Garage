9040a428fefe1f3628c9f955cb14b654388220fc
NararyaGarage <nararyagaragee@gmail.com>
2025-05-01 07:37:23 +0700

    Add files via upload
    Signed-off-by: NararyaGarage <nararyagaragee@gmail.com>

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,1 +1,8 @@
+import React from 'react';
+import DiscordLayout from '@/components/discord/DiscordLayout';
 
+const Home: React.FC = () => {
+  return <DiscordLayout />;
+};
+
+export default Home;
